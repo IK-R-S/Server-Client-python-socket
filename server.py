3e58b051a04767269c5cd97e369350f5de2970d8
Kauan Sena <lkrs@protonmail.com>
2023-12-31 19:52:34 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,30 +1,31 @@
 import socket
+import time
 
-# Declarando variáveis de conexão para subir o servidor
-HOST = 'localhost' # Endereço do servidor (esta máquina)
-PORT = 8888 # Porta de conexão
-server = (HOST, PORT)
+SERVER_HOST = '192.168.15.10'
+SERVER_PORT = 8888
 
-# Declarando tipo de cconexão TCP IPV4 com socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Combinando endereço e porta do servidor na variável s do socket
-s.bind(server)
+def connect_to_server():
+    while True:
+        try:
+            print("\nAGUARDANDO RESPOSTA DO SERVIDOR...")
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((SERVER_HOST, SERVER_PORT))
+            return s
+        except (socket.error, socket.timeout):
+            print(f"\n\033[31mSem resposta, enviando nova requisição ao servidor em {SERVER_HOST}:{SERVER_PORT}\033[0;0;0m")
+            time.sleep(2)
 
-# Socket em escuta
-s.listen()
-print("\nAGUARDANDO CONEXÃO...")
+# Inicia a tentativa de conexão
+client_socket = connect_to_server()
 
-# Aceitando conexão do cliente
-clientConn, clientAddress = s.accept()
-print(f"\nCONEXÃO DE {clientAddress}")
+# Enviando dados ao servidor
+packet = 'Testando conexão'
+client_socket.sendall(str.encode(packet))
 
-while True:
-    data = clientConn.recv(1024)
-    if not data:
-        print("CONEXÃO ENCERRADA")
-        clientConn.close()
-        break
-    else:
-        dataContent = data.decode('utf-8')
-        print('DADOS RECEBIDOS DO CLIENTE: ', dataContent)
-        clientConn.sendall(str.encode('SERVER OK - STATUS 200'))
+# Recebendo dados do servidor
+serverResponse = client_socket.recv(1024)
+response = serverResponse.decode('utf-8')
+print(f'\n\033[1;34mRESPOSTA RECEBIDA:\n\033[0;0;0m{response}\n')
+
+# Encerrando socket de conexão com o servidor
+client_socket.close()
